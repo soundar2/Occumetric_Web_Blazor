e7594084f8b0bb744f17352b9d99b449ce79a9b9
soundar2 <soundar2@gmail.com>
2020-08-29 22:43:04 +0530

    Update Task working

--- Shared/JobTasks/UpdateJobTaskDto.cs
+++ Shared/JobTasks/UpdateJobTaskDto.cs
@@ -2,6 +2,11 @@
 {
     public class UpdateJobTaskDto
     {
+        public UpdateJobTaskDto()
+        {
+            EffortType = string.Empty;
+        }
+
         public int Id { get; set; }
         public string Name { get; set; }
 
